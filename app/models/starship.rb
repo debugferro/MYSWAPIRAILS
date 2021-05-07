24af38457ab81f4250f594aee7e20da189f37fc8
Gabriel Fontes <gabrielferroroque@hotmail.com>
2021-05-07 16:13:09 -0300

    [FEATURE] Added Starship model, validations and fields

---
+++ app/models/starship.rb
@@ -0,0 +1,25 @@
+class Starship
+  include Mongoid::Document
+  include Mongoid::Timestamps
+
+  field :name, type: String
+  field :model, type: String
+  field :startship_class, type: String
+  field :manufacturer, type: String
+  field :cost_in_credits, type: String
+  field :length, type: String
+  field :crew, type: String
+  field :passengers, type: String
+  field :max_atmosphering_speed, type: String
+  field :hyperdrive_rating, type: String
+  field :MGLT, type: String
+  field :cargo_capacity, type: String
+  field :consumables, type: String
+  field :url, type: String
+
+  validates :name, uniqueness: true
+  validates :url, uniqueness: true
+
+  has_and_belongs_to_many :pilots, class_name: 'People', inverse_of: :starships
+  has_and_belongs_to_many :films, class_name: 'Film', inverse_of: :starships
+end
