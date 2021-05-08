ec5be3b2dde1ad972fde2bc4753b5e0d6c5eeaaa
Gabriel Fontes <gabrielferroroque@hotmail.com>
2021-05-08 04:06:50 -0300

    [FEATURE] Added mapping methods for planet and species model.

--- app/models/planet.rb
+++ app/models/planet.rb
@@ -5,6 +5,7 @@ class Planet
   field :name, type: String
   field :diameter, type: String
   field :rotation_period, type: String
+  field :orbital_period, type: String
   field :gravity, type: String
   field :population, type: String
   field :climate, type: String
@@ -17,6 +18,32 @@ class Planet
 
   has_many :residents, class_name: 'People', inverse_of: :homeworld
   has_and_belongs_to_many :films, class_name: 'Film', inverse_of: :planets
-  # Residents = > People
-  # Films
+
+  def self.map_data(people)
+    people.map(&:map_info)
+  end
+
+  def map_info
+    {
+      name: name,
+      id: id.to_s,
+      diameter: diameter,
+      rotation_period: rotation_period,
+      orbital_period: orbital_period,
+      gravity: gravity,
+      population: population,
+      climate: climate,
+      terrain: terrain,
+      surface_water: surface_water,
+      residents: residents.map(&:slim_map_info),
+      films: films.map(&:slim_map_info),
+    }
+  end
+
+  def slim_map_info
+    {
+      name: name,
+      id: id.to_s
+    }
+  end
 end
--- app/models/species.rb
+++ app/models/species.rb
@@ -4,7 +4,7 @@ class Species
 
   field :name, type: String
   field :classification, type: String
-  field :deisgnation, type: String
+  field :designation, type: String
   field :average_height, type: String
   field :average_lifespan, type: String
   field :eye_colors, type: String
@@ -19,4 +19,33 @@ class Species
   has_and_belongs_to_many :people, class_name: 'People', inverse_of: :species
   has_and_belongs_to_many :films, class_name: 'Film', inverse_of: :species
   has_one :homeworld, class_name: 'Planet'
+
+  def self.map_data(species)
+    species.map(&:map_info)
+  end
+
+  def map_info
+    {
+      name: name,
+      id: id.to_s,
+      classification: classification,
+      designation: designation,
+      average_height: average_height,
+      average_lifespan: average_lifespan,
+      eye_colors: eye_colors,
+      hair_colors: hair_colors,
+      skin_colors: skin_colors,
+      language: language,
+      homeworld: homeworld&.slim_map_info,
+      films: films.map(&:slim_map_info),
+      people: people.map(&:slim_map_info),
+    }
+  end
+
+  def slim_map_info
+    {
+      name: name,
+      id: id.to_s
+    }
+  end
 end
